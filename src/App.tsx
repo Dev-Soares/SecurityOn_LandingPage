0f5ce3cad1f5f99205b8c80b3a0a1e3372838f18
Bernardo <bernardo.soares30@outlook.com>
2026-03-17 10:08:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
+      <h1 className="text-4xl font-bold text-center pt-20">SecurityOn Landing</h1>
+      <p className="text-center mt-4 text-gray-500">Em construcao...</p>
+    </div>
+  )
+}
+
+export default App
